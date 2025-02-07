52fd5362680b1af427cf8b0bbed33088fa2f98a2
Sipurit <gun0820303553@gmail.com>
2025-02-07 20:40:19 +0700

    #

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,18 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(Rect &R,Rect &T){
-	if(R.x+R.w < T.x || R.y+R.h < T.y){
-		return 0;
-	}
-	else{
-		if(R.x+R.w > T.x+T.w && R.y+R.w > T.y+T.h){
-			return T.w*T.h;
-		}
-		else return ((R.x+R.w)-T.x)*((R.y+R.h)-T.y);
-	}
+double overlap(Rect R1, Rect R2) {
+    double x_overlap = max(0.0, min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x));
+    double y_overlap = max(0.0, min(R1.y, R2.y) - max(R1.y - R1.h, R2.y - R2.h));
+    return x_overlap * y_overlap;
 }
