0710e5e65e58b9452b3c75279bbee497c5e4f71a
Sipurit <gun0820303553@gmail.com>
2025-02-07 15:32:07 +0700

    #

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect &R,Rect &T){
+	if(R.x+R.w < T.x || R.y+R.h < T.y){
+		return 0;
+	}
+	else{
+		if(R.x+R.w > T.x+T.w && R.y+R.w > T.y+T.h){
+			return T.w*T.h;
+		}
+		else return ((R.x+R.w)-T.x)*((R.y+R.h)-T.y);
+	}
 }
